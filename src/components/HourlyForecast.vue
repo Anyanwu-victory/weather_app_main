0fc2564b852b0942d1b1d8883a3c8caa33ecbfe5
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2025-12-21 19:32:02 +0100

    Update HourlyForecast.vue

--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,9 +1,18 @@
 <script setup>
 
+const hours = [
+  { time: "3 PM", temperature: 20,},
+  { time: "4 PM", temperature: 21,},
+  { time: "12 PM", temperature: 24, },
+  { time: "11 AM", temperature: 25, },
+  { time: "5 PM", temperature: 21,},
+  { time: "6 PM", temperature: 25,},
+  { time: "8 PM", temperature: 24,},
+];
 </script>
 
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-5">
+  
   
     <!-- Right section -->
 
@@ -11,7 +20,8 @@
       <div class="rounded-2xl bg-neutral-800 p-4 space-y-4">
         <div class="flex justify-between items-center">
           <h3 class="font-semibold">Hourly forecast</h3>
-          <select class="bg-neutral-700 rounded-lg px-2 py-1 text-sm">
+
+          <select class="bg-neutral-700 rounded-lg px-2 py-2 text-sm">
             <option>Tuesday</option>
           </select>
         </div>
@@ -19,14 +29,12 @@
         <div class="space-y-3">
           <div v-for="hour in hours" :key="hour"
             class="flex justify-between items-center bg-neutral-700/40 rounded-xl px-4 py-3">
-            <span>3 PM</span>
-            <span>20°</span>
+            <div class=""></div>
+            <span>{{ hour.time }}</span>
+            <span>{{ hour.temperature }}°</span>
           </div>
         </div>
       </div>
-
-
-  </div>
 
 </template>
 
