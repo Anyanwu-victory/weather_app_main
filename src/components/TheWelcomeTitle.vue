5c8dd662bacd765a405b70fe63bd958d11d408d4
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2025-12-21 16:47:38 +0100

    The welcome title

---
+++ src/components/TheWelcomeTitle.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- Welcome text -->
+  <div class="justify-center flex items-center">
+    <span>
+      <h1 class="text-[30px] font-bold py-9 md:text-[40px]">How's the sky looking today?</h1>
+    </span>
+  </div>
+  <!-- Welcome text -->
+</template>
